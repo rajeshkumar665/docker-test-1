21c3680f2b56dd3d2c6b1bff31f57343e5bcd108
Rajesh <rajeshkumard401@gmail.com>
2025-04-30 01:33:11 +0530

    test-v1

--- src/main/java/com/dockerapp/controller/HelloWorldController.java
+++ src/main/java/com/dockerapp/controller/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.dockerapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @GetMapping("/message")
     public String getMessage(){
         return "Hello-world Rajesh";
     }
